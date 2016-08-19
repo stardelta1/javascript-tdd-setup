d09c05eb410c68b59d3580f1991fc327a68eaf20
Star <emeshilisylvester@gmail.com>
2016-08-19 04:33:54 -1100

    Working commit

--- tests.js
+++ tests.js
@@ -3,22 +3,83 @@
 var chai = require('chai');
 var assert = chai.assert;
 
-var lib = require('./lib/library.js');
+var Note = require('./lib/notes.js');
+var NotesApplication = require('./lib/notesapplication.js');
 
-describe("Test that constants are computed properly", function() {
-  it("should give 10, 4 for constants 2, 5 as a and n respectively", function() {
-    assert(
-      lib.compareCoefficients(lib.quadraticDerivative(2, 5), { a: 10, n: 4 })
-    );
-  });
-  it("should give 2, 1 for constants 1, 2 as a and n respectively", function() {
-    assert(
-      lib.compareCoefficients(lib.quadraticDerivative(1, 2), { a: 2, n: 1 })
-    );
-  });
-  it("should give 8, 1 for constants 4, 2 as a and n respectively", function() {
-    assert(
-      lib.compareCoefficients(lib.quadraticDerivative(4, 2), { a: 8, n: 1 })
-    );
-  });
+
+describe("Note creation works properly", function() {
+    it("assigns author based on the parameter supplied in the constructor", function() {
+        var note = new Note("Hello world", "Star");
+        var noteapp = new NotesApplication();
+        assert(note.author === "Star");
+        assert(note.content === "Hello world");
+    });
+});
+
+describe("Notes application manipulates the note", function() {
+    it("increments the note list as notes are added", function() { 
+        var note = new Note("Hello world", "Star");
+        var noteapp = new NotesApplication();
+        assert(noteapp.addNote(note) === 1);
+    });
+    it("add note accepts note object as a parameter", function() {
+        var note = new Note("Hello world", "Star");
+        var noteapp = new NotesApplication();
+        assert(note.hasOwnProperty('author') && note.hasOwnProperty('content'));
+    });
+    it("Get a note with its id", function() {
+        var note = new Note("Hello world", "Star");
+        var note2 = new Note("Howdy people", "Kels");
+        var noteapp = new NotesApplication();
+        noteapp.addNote(note);
+        assert(noteapp.get(0) === "Hello world");
+    });
+    it("Delete a note with its id", function() {
+        var note = new Note("Hello world", "Star");
+        var note2 = new Note("Howdy people", "Kels");
+        var noteapp = new NotesApplication();
+        noteapp.addNote(note);
+        assert(noteapp.delete(0) === true);
+    });
+    it("Search for a string and return its value", function() {
+        var note = new Note("Hello world", "Star");
+        var note2 = new Note("Howdy people", "Kels");
+        var noteapp = new NotesApplication();
+        noteapp.addNote(note);
+        assert(noteapp.search("Hello world") === "Hello world");
+    });
+    it("Pass in the text and note id as parameter and edit the text of the former content", function() {
+        var note = new Note("Hello world", "Star");
+        var note2 = new Note("Howdy people", "Kels");
+        var noteapp = new NotesApplication();
+        noteapp.addNote(note);
+        assert(noteapp.edit(0,"Welcome world") === "Welcome world");
+    });
+    it("Get notes count", function() {
+        var note = new Note("Hello world", "Star");
+        var note2 = new Note("Howdy people", "Kels");
+        var noteapp = new NotesApplication();
+        noteapp.addNote(note);
+        noteapp.addNote(note2);
+        assert(noteapp.notesCount() > 0);
+    });
+    it("Check if the note list is empty on instantiation", function() {
+        var note = new Note("Hello world", "Star");
+        var note2 = new Note("Howdy people", "Kels");
+        var noteapp = new NotesApplication();
+        assert(noteapp.notesList.length === 0);
+    });
+    it("Check if parameter passed is an instance of Note", function() {
+        var note = new Note("Hello world", "Kels");
+        var noteapp = new NotesApplication();
+        noteapp.addNote(note);
+        assert(note instanceof Note);
+    });
+    it("Get all the notes list", function() {
+        var note = new Note("Hello world", "Star");
+        var noteapp = new NotesApplication();
+        noteapp.addNote(note);
+        assert(noteapp.listNotes() === "\nNote ID: "+ 0 +"\nHello world\nBy Author Star\n");
+    });
+
 });
